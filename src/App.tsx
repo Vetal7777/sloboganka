39e5cf21b3c95ae38c746fc55b1bd7827b29c22d
Kuznetsov Vitalii <jobschildren@gmail.com>
2022-10-11 01:45:22 +0300

    main/test for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import API from "./utils/API";
 
 function App() {
+  const [state,setState] = useState('')
+  useEffect(() => {
+    API.get('header')
+        .then(({data}) => setState(data[0].title))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {state}
+    </>
   );
 }
 
