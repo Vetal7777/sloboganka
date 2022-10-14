97c4330b93cb4fe394e57d0f653b7753f38a55fe
Kuznetsov Vitalii <jobschildren@gmail.com>
2022-10-14 11:44:21 +0300

    main/add pics

--- src/components/good/good.tsx
+++ src/components/good/good.tsx
@@ -4,6 +4,10 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 export default function Good({item}:{item:GoodI}){
+    const pics = new Array(item.pic_length)
+        .fill('')
+        .map((elem,index) => `${item.id}-${index + 1}.png`);
+    console.log(pics)
     return (
         <>
             <div className={styles.container}>
@@ -14,7 +18,12 @@ export default function Good({item}:{item:GoodI}){
                     infiniteLoop={true}
                     showIndicators={false}
                 >
-
+                    {pics.map(picture => (
+                        <img
+                            src={`./assets/images/${item.id}/${picture}`}
+                            className={styles.slide}
+                        />
+                    ))}
                 </Carousel>
                 <div className={styles.content}>
                     <h3
